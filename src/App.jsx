c9fd7e3912d574b78af02c65d7c8d400c3d3d2a2
Engineer-Ano <iam.a.shubladze@gmail.com>
2025-11-16 18:06:38 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: "40px", fontSize: "24px" }}>
+      <h1>StudG</h1>
+      <p>Your website is deployed successfully 🎉</p>
+    </div>
+  )
+}
